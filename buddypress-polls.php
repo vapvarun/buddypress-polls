2c6226d67c8d2a7058ad9e17c8ecb1c80cb4c90a
Varun Kumar Dubey <vapvarun@gmail.com>
2020-05-13 00:07:31 +0530

    Update buddypress-polls.php

--- buddypress-polls.php
+++ buddypress-polls.php
@@ -16,7 +16,7 @@
  * Plugin Name:       BuddyPress Polls
  * Plugin URI:        http://www.wbcomdesigns.com/plugins/
  * Description:       BuddyPress Polls plugin allows you and your community to create polls in posts. The polls can be placed in the main activity stream, in users’ profiles and even in groups.
- * Version:           2.5.0
+ * Version:           2.6.0
  * Author:            wbcomdesigns
  * Author URI:        https://wbcomdesigns.com
  * License:           GPL-2.0+
@@ -36,7 +36,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Rename this for your plugin and update it as you release new versions.
  */
  if( ! defined( 'BPOLLS_PLUGIN_VERSION' ) )
-			define( 'BPOLLS_PLUGIN_VERSION', '2.5.0' );
+			define( 'BPOLLS_PLUGIN_VERSION', '2.6.0' );
 
  if( ! defined( 'BPOLLS_PLUGIN_FILE' ) )
 			define( 'BPOLLS_PLUGIN_FILE', __FILE__ );
