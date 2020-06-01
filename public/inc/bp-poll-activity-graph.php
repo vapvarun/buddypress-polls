45445a029b561d0d9b8554c7afd2f21747a64f69
pwpdev <pramod@wbcomdesigns.com>
2020-06-01 18:10:20 +0530

    (#43)Fixed trim poll title

--- public/inc/bp-poll-activity-graph.php
+++ public/inc/bp-poll-activity-graph.php
@@ -15,72 +15,77 @@ defined( 'ABSPATH' ) || exit;
  *
  * @since 1.0.0
  */
-//function bpolls_register_poll_graph_widgets() {
-	add_action( 'widgets_init', function() { register_widget( 'BP_Poll_Activity_Graph_Widget' ); } );
-//}
-//add_action( 'bp_register_widgets', 'bpolls_register_poll_graph_widgets' );
+// function bpolls_register_poll_graph_widgets() {
+	add_action(
+		'widgets_init',
+		function() {
+			register_widget( 'BP_Poll_Activity_Graph_Widget' );
+		}
+	);
+	// }
+	// add_action( 'bp_register_widgets', 'bpolls_register_poll_graph_widgets' );
 
-function bpolls_activity_graph_ajax(){
-	if ( isset( $_POST[ 'action' ] ) && $_POST[ 'action' ] == 'bpolls_activity_graph_ajax' ) {
+	function bpolls_activity_graph_ajax() {
+		if ( isset( $_POST['action'] ) && $_POST['action'] == 'bpolls_activity_graph_ajax' ) {
 			check_ajax_referer( 'bpolls_widget_security', 'ajax_nonce' );
 			$activity_id = $_POST['actid'];
 
-			$activity_details = bp_activity_get_specific( $args = array('activity_ids'=>$activity_id) );
+			$activity_details = bp_activity_get_specific( $args = array( 'activity_ids' => $activity_id ) );
 
-			if(is_array($activity_details)){
-				$poll_title = $activity_details['activities'][0]->content;
-			}else{
+			if ( is_array( $activity_details ) ) {
+				$poll_title = wp_trim_words( $activity_details['activities'][0]->content, 5, '...' );
+			} else {
 				$poll_title = '';
 			}
-			
 
-			$activity_meta = bp_activity_get_meta( $activity_id, 'bpolls_meta');
+			$activity_meta = bp_activity_get_meta( $activity_id, 'bpolls_meta' );
 
-			$poll_options = isset($activity_meta['poll_option'])?$activity_meta['poll_option']:'';
-			
-			if( !empty($poll_options) && is_array($poll_options) ){
-				foreach ($poll_options as $key => $value) {
-			
-					if( isset( $activity_meta['poll_total_votes'] ) ){
+			$poll_options = isset( $activity_meta['poll_option'] ) ? $activity_meta['poll_option'] : '';
+
+			if ( ! empty( $poll_options ) && is_array( $poll_options ) ) {
+				foreach ( $poll_options as $key => $value ) {
+
+					if ( isset( $activity_meta['poll_total_votes'] ) ) {
 						$total_votes = $activity_meta['poll_total_votes'];
-					}else{
+					} else {
 						$total_votes = 0;
 					}
 
-					if( isset( $activity_meta['poll_optn_votes'] ) && array_key_exists( $key, $activity_meta['poll_optn_votes'] ) ){
-						$this_optn_vote = $activity_meta['poll_optn_votes'][$key];
-					}else{
+					if ( isset( $activity_meta['poll_optn_votes'] ) && array_key_exists( $key, $activity_meta['poll_optn_votes'] ) ) {
+						$this_optn_vote = $activity_meta['poll_optn_votes'][ $key ];
+					} else {
 						$this_optn_vote = 0;
 					}
 
-					if( $total_votes != 0 ){
-						$vote_percent = round( $this_optn_vote/$total_votes*100, 2 );
-					}else{
+					if ( $total_votes != 0 ) {
+						$vote_percent = round( $this_optn_vote / $total_votes * 100, 2 );
+					} else {
 						$vote_percent = '(no votes yet)';
 					}
 
 					$bpolls_votes_txt = $this_optn_vote . '&nbsp;of&nbsp;' . $total_votes;
 
-					$uptd_votes[$activity_id][] = array(
+					$uptd_votes[ $activity_id ][] = array(
 						'poll_title' => $poll_title,
-						'label' => $value,
-						'y' => $vote_percent,
-						'color' => bpolls_color()
+						'label'      => $value,
+						'y'          => $vote_percent,
+						'color'      => bpolls_color(),
 
 					);
 				}
 			}
 
-			echo json_encode($uptd_votes);die;
+			echo json_encode( $uptd_votes );
+			die;
 		}
-}
-add_action( 'wp_ajax_bpolls_activity_graph_ajax','bpolls_activity_graph_ajax' );
-add_action( 'wp_ajax_nopriv_bpolls_activity_graph_ajax', 'bpolls_activity_graph_ajax' );
+	}
+	add_action( 'wp_ajax_bpolls_activity_graph_ajax', 'bpolls_activity_graph_ajax' );
+	add_action( 'wp_ajax_nopriv_bpolls_activity_graph_ajax', 'bpolls_activity_graph_ajax' );
 
-function bpolls_color(){
-	return '#'.random_color_part() . random_color_part() . random_color_part();
-}
+	function bpolls_color() {
+		return '#' . random_color_part() . random_color_part() . random_color_part();
+	}
 
-function random_color_part(){
-	return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
-}
+	function random_color_part() {
+		return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT );
+	}
--- public/js/poll-activity-graph.js
+++ public/js/poll-activity-graph.js
@@ -1,84 +1,89 @@
-(function( $ ) {
-	'use strict';
-var load_Chart;
-var ajax_Chart;
-window.onload = function () {
-	$('.poll-bar-chart').each(function(){
-		var act_id = $(this).data('id');
-		var id = $(this).attr('id');
-		var res = JSON.parse(bpolls_wiget_obj.votes);
-		if( res[act_id] ) {
-			var arr_label = [];
-			var arr_per = [];
-			var arr_color = [];
-			$.each(res[act_id], function(i, item) {
-				arr_label.push(item.label.toString());
-				arr_per.push(item.y.toString());
-				arr_color.push(item.color);
-			});
+(function($) {
+  'use strict';
+  var load_Chart;
+  var ajax_Chart;
+  window.onload = function() {
+    $('.poll-bar-chart').each(function() {
+      var act_id = $(this).data('id');
+      var id = $(this).attr('id');
+      var res = JSON.parse(bpolls_wiget_obj.votes);
+      if (res[act_id]) {
+        var arr_label = [];
+        var arr_per = [];
+        var arr_color = [];
+        $.each(res[act_id], function(i, item) {
+          arr_label.push(item.label.toString());
+          arr_per.push(item.y.toString());
+          arr_color.push(item.color);
+        });
 
-			load_Chart = new Chart($("#"+id), {
-				type: 'pie',
-				data: {
-					labels: arr_label,
-					datasets: [{
-						label: "Poll Activity Graph (%)",
-						backgroundColor: arr_color,
-						data: arr_per
-					}]
-				},
-				options: {
-					title: {
-						display: true,
-						text: res[act_id][0].poll_title
-					}
-				}
-			});
-		}
+        load_Chart = new Chart($("#" + id), {
+          type: 'pie',
+          data: {
+            labels: arr_label,
+            datasets: [{
+              label: "Poll Activity Graph (%)",
+              backgroundColor: arr_color,
+              data: arr_per
+            }]
+          },
+          options: {
+            title: {
+              display: true,
+              text: res[act_id][0].poll_title
+            }
+          }
+        });
+      }
 
-	});
-}
+    });
+  }
 
-$( document ).on(
-	'change', '.bpolls-activities-list', function () {
-		var actid = $( this ).find( ":selected" ).val();
-		var clickd_obj = $(this);
-		var data = {
-					'action': 'bpolls_activity_graph_ajax',
-					'actid' : actid,
-					'ajax_nonce': bpolls_wiget_obj.ajax_nonce
-				};
+  $(document).on(
+    'change', '.bpolls-activities-list',
+    function() {
+      var actid = $(this).find(":selected").val();
+      var clickd_obj = $(this);
+      var data = {
+        'action': 'bpolls_activity_graph_ajax',
+        'actid': actid,
+        'ajax_nonce': bpolls_wiget_obj.ajax_nonce
+      };
 
-		$.post( bpolls_wiget_obj.ajax_url, data, function ( response ) {
-			clickd_obj.parents().siblings('.poll-bar-chart').remove();
-			$('<canvas class="poll-bar-chart" data-id="'+actid+'" id="bpolls-activity-chart-'+actid+'"></canvas>').insertAfter( clickd_obj.parents('.bpolls-activity-select') );
-			var resp = JSON.parse(response);
-			var arr2_label = [];
-			var arr2_per = [];
-			var arr2_color = [];
-			$.each(resp[actid], function(i, item) {
-				arr2_label.push(item.label.toString());
-				arr2_per.push(item.y.toString());
-				arr2_color.push(item.color);
-			});
-			ajax_Chart = new Chart(clickd_obj.parents().siblings('.poll-bar-chart'), {
-			type: 'pie',
-			data: {
-				labels: arr2_label,
-				datasets: [{
-					label: "Poll Activity Graph (%)",
-					backgroundColor: arr2_color,
-					data: arr2_per
-				}]
-			},
-			options: {
-				title: {
-					display: true,
-					text: resp[actid][0].poll_title
-				}
-			}
-			});
-		} );
-	}
-);
-})( jQuery );
+      $.post(bpolls_wiget_obj.ajax_url, data, function(response) {
+        clickd_obj.parents().siblings('.poll-bar-chart').remove();
+        $('<canvas class="poll-bar-chart" data-id="' + actid + '" id="bpolls-activity-chart-' + actid + '"></canvas>').insertAfter(clickd_obj.parents('.bpolls-activity-select'));
+        var resp = JSON.parse(response);
+        var arr2_label = [];
+        var arr2_per = [];
+        var arr2_color = [];
+        var arr2_title = [];
+        var title = '';
+        $.each(resp[actid], function(i, item) {
+          arr2_label.push(item.label.toString());
+          arr2_per.push(item.y.toString());
+          arr2_color.push(item.color);
+          title = item.poll_title;
+        });
+
+        ajax_Chart = new Chart(clickd_obj.parents().siblings('.poll-bar-chart'), {
+          type: 'pie',
+          data: {
+            labels: arr2_label,
+            datasets: [{
+              label: "Poll Activity Graph (%)",
+              backgroundColor: arr2_color,
+              data: arr2_per
+            }]
+          },
+          options: {
+            title: {
+              display: true,
+              text: title
+            }
+          }
+        });
+      });
+    }
+  );
+})(jQuery);
