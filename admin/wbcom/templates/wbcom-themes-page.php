dc591d76d015ad5ba1e4e50550cb5bb176813f7d
Wbcom dev <pallavi@wbcomdesigns.com>
2021-04-14 17:06:49 +0530

    Update Wbcom themes preview urls

--- admin/wbcom/templates/wbcom-themes-page.php
+++ admin/wbcom/templates/wbcom-themes-page.php
@@ -11,17 +11,17 @@
 					<div class="wbcom-demo-title">
 						<h2><?php esc_html_e( 'REIGN Community', 'buddypress-polls' ); ?></h2>
 						<ul class="wbcom_theme_features_list">
-                            <li><?php esc_html_e( 'Multiple Member Directory Layout', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Multiple Member Header Layout', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Multiple Group Directory Layout', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Multiple Group Header Layout', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'BuddyPress Customization Options', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'BuddyPress Extra Widgets', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Inbuilt Social Profile Module', 'buddypress-polls' ); ?></li>
-                        </ul>
+							<li><?php esc_html_e( 'Multiple Member Directory Layout', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Multiple Member Header Layout', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Multiple Group Directory Layout', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Multiple Group Header Layout', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'BuddyPress Customization Options', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'BuddyPress Extra Widgets', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Inbuilt Social Profile Module', 'buddypress-polls' ); ?></li>
+						</ul>
 						<div class="wbcom-middle">
 							<a href="https://wbcomdesigns.com/downloads/reign-buddypress-theme/#pricing-table-leranmate" class="wbcom-button wbcom-purchase" target="_blank"><?php esc_html_e( 'Purchase', 'buddypress-polls' ); ?></a>
-							<a target="_blank" href="http://demos.wbcomdesigns.com/reign-demo1/" class="wbcom-button"><?php esc_html_e( 'Preview', 'buddypress-polls' ); ?></a>
+							<a target="_blank" href="http://demos.wbcomdesigns.com/" class="wbcom-button"><?php esc_html_e( 'Preview', 'buddypress-polls' ); ?></a>
 						</div>
 					</div>
 				</div>
@@ -37,17 +37,17 @@
 					<div class="wbcom-demo-title">
 						<h2><?php esc_html_e( 'Learnmate', 'buddypress-polls' ); ?></h2>
 						<ul class="wbcom_theme_features_list">
-                            <li><?php esc_html_e( 'Add Review to your course', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Course Coming Soon', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Grid And List Layout', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Learndash/LifterLMS Extra Widgets', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Unparalleled Course Layout', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Awesome Learndash/LifterLMS Pages', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Distraction Free Reading', 'buddypress-polls' ); ?></li>
-                        </ul>
+							<li><?php esc_html_e( 'Add Review to your course', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Course Coming Soon', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Grid And List Layout', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Learndash/LifterLMS Extra Widgets', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Unparalleled Course Layout', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Awesome Learndash/LifterLMS Pages', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Distraction Free Reading', 'buddypress-polls' ); ?></li>
+						</ul>
 						<div class="wbcom-middle">
 							<a href="https://wbcomdesigns.com/downloads/learnmate-learndash/#pricing-table-leranmate" class="wbcom-button wbcom-purchase" target="_blank"><?php esc_html_e( 'Purchase', 'buddypress-polls' ); ?></a>
-							<a target="_blank" href="https://wpbp.in/learndash/" class="wbcom-button" ><?php esc_html_e( 'Preview', 'buddypress-polls' ); ?></a>
+							<a target="_blank" href="https://installer.wbcomdesigns.com/buddypress-social-learning/" class="wbcom-button" ><?php esc_html_e( 'Preview', 'buddypress-polls' ); ?></a>
 						</div>
 					</div>
 				</div>
@@ -63,15 +63,15 @@
 					<div class="wbcom-demo-title">
 						<h2><?php esc_html_e( 'StoreMate Dokan', 'buddypress-polls' ); ?></h2>
 						<ul class="wbcom_theme_features_list">
-                            <li><?php esc_html_e( 'Dokan Extra Widgets', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Unique Single Product Page Customization', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Awesome WooCommerce Pages', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Grid And List Layout', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Automated Mega Menu Support', 'buddypress-polls' ); ?></li>
-                        </ul>
+							<li><?php esc_html_e( 'Dokan Extra Widgets', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Unique Single Product Page Customization', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Awesome WooCommerce Pages', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Grid And List Layout', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Automated Mega Menu Support', 'buddypress-polls' ); ?></li>
+						</ul>
 						<div class="wbcom-middle">
 							<a target="_blank" href="https://wbcomdesigns.com/downloads/storemate-dokan/#pricing-table-leranmate" class="wbcom-button wbcom-purchase"><?php esc_html_e( 'Purchase', 'buddypress-polls' ); ?></a>
-							<a target="_blank" href="https://demos.wbcomdesigns.in/reign-dokan/" class="wbcom-button"><?php esc_html_e( 'Preview', 'buddypress-polls' ); ?></a>
+							<a target="_blank" href="https://installer.wbcomdesigns.com/storemate-woocommerce/" class="wbcom-button"><?php esc_html_e( 'Preview', 'buddypress-polls' ); ?></a>
 						</div>
 					</div>
 				</div>
@@ -87,14 +87,14 @@
 					<div class="wbcom-demo-title">
 						<h2><?php esc_html_e( 'Jobmate Theme', 'buddypress-polls' ); ?></h2>
 						<ul class="wbcom_theme_features_list">
-                            <li><?php esc_html_e( 'Simple live searching and filtering', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Unique Job listing Page', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Awesome Resume list Page', 'buddypress-polls' ); ?></li>
-                            <li><?php esc_html_e( 'Outstanding all job maanger pagesLayout', 'buddypress-polls' ); ?></li>
-                        </ul>
+							<li><?php esc_html_e( 'Simple live searching and filtering', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Unique Job listing Page', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Awesome Resume list Page', 'buddypress-polls' ); ?></li>
+							<li><?php esc_html_e( 'Outstanding all job maanger pagesLayout', 'buddypress-polls' ); ?></li>
+						</ul>
 						<div class="wbcom-middle">
 							<a target="_blank" href="https://wbcomdesigns.com/downloads/jobmate-theme/" class="wbcom-button wbcom-purchase"><?php esc_html_e( 'Purchase', 'buddypress-polls' ); ?></a>
-							<a target="_blank" href="http://demos.wbcomdesigns.in/jobmate-theme/" class="wbcom-button"><?php esc_html_e( 'Preview', 'buddypress-polls' ); ?></a>
+							<a target="_blank" href="https://installer.wbcomdesigns.com/reign-jobmate-bb-platform/" class="wbcom-button"><?php esc_html_e( 'Preview', 'buddypress-polls' ); ?></a>
 						</div>
 					</div>
 				</div>
