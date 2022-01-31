064e7b1f07a66e332706d01ed6e05905a98e7835
dishantpatel <dishant.patel1007@gmail.com>
2022-01-31 15:42:53 +0530

    Fixed - BB Plattfom related script only work when buddyboss platform plugin activate

--- public/js/buddypress-polls-public.js
+++ public/js/buddypress-polls-public.js
@@ -77,42 +77,44 @@
 			}
 		});
 		
-		var bb_polls_Interval;
 		
-		function bb_pools_icon_push() {
+		if (bpolls_ajax_object.buddyboss ) {
+			var bb_polls_Interval;
 			
-			bb_polls_Interval = setInterval( function() {
-				if (bpolls_ajax_object.buddyboss && $('#whats-new-form:not(.focus-in) #whats-new-toolbar .bpolls-html-container-placeholder').length == 0 ){				
-					$('#whats-new-form:not(.focus-in) #whats-new-toolbar').append('<div class="post-elements-buttons-item bpolls-html-container-placeholder"><span class="bpolls-icon bp-tooltip" data-bp-tooltip-pos="up" data-bp-tooltip="Add a poll"><i class="fa fa-bar-chart"></i></span></div>');					
-				}
-			}, 1000);
-			
-		}
-			
-		bb_pools_icon_push();
-		
-		$(document).on('click', '.bb-model-close-button, .activity-update-form-overlay', function(){			
-			clearInterval(bb_polls_Interval);
+			function bb_pools_icon_push() {
+				
+				bb_polls_Interval = setInterval( function() {
+					if (bpolls_ajax_object.buddyboss && $('#whats-new-form:not(.focus-in) #whats-new-toolbar .bpolls-html-container-placeholder').length == 0 ){				
+						$('#whats-new-form:not(.focus-in) #whats-new-toolbar').append('<div class="post-elements-buttons-item bpolls-html-container-placeholder"><span class="bpolls-icon bp-tooltip" data-bp-tooltip-pos="up" data-bp-tooltip="Add a poll"><i class="fa fa-bar-chart"></i></span></div>');					
+					}
+				}, 1000);
+				
+			}
+				
 			bb_pools_icon_push();
-		});
-		
-		/* jQuery Ajax prefilter*/
-		$.ajaxPrefilter( function( options, originalOptions, jqXHR ) {			
-			try {
-				if ( originalOptions.data == null || typeof ( originalOptions.data ) == 'undefined' || typeof ( originalOptions.data.action ) == 'undefined' ) {
+			
+			$(document).on('click', '.bb-model-close-button, .activity-update-form-overlay', function(){			
+				clearInterval(bb_polls_Interval);
+				bb_pools_icon_push();
+			});
+			
+			/* jQuery Ajax prefilter*/
+			$.ajaxPrefilter( function( options, originalOptions, jqXHR ) {			
+				try {
+					if ( originalOptions.data == null || typeof ( originalOptions.data ) == 'undefined' || typeof ( originalOptions.data.action ) == 'undefined' ) {
+						return true;
+					}
+				} catch ( e ) {
 					return true;
 				}
-			} catch ( e ) {
-				return true;
-			}
-			
-			if ( originalOptions.data.action == 'post_update' ) {
-				clearInterval(bb_polls_Interval);
-				bb_pools_icon_push();				
-			}
-			
-		} );
-
+				
+				if ( originalOptions.data.action == 'post_update' ) {
+					clearInterval(bb_polls_Interval);
+					bb_pools_icon_push();				
+				}
+				
+			} );
+		}
 
 		$(document).on( 'focus', '#whats-new', function() {
 			$('#whats-new-options').addClass('bpolls-rtm-class');
