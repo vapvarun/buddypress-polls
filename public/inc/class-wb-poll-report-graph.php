8794ca15cd4916704411935a281708d12bf187ed
Aman kumar <pallavi@wbcomdesigns.com>
2023-07-04 18:34:37 +0530

    (#298) poll results do not display on bb platform

--- public/inc/class-wb-poll-report-graph.php
+++ public/inc/class-wb-poll-report-graph.php
@@ -36,9 +36,34 @@ class Wb_Poll_Report extends WP_Widget {
 			echo WBPollHelper::show_backend_single_poll_widget_result_all_voted( esc_html($poll_id), 'shortcode', 'text' );
 		}else{
 			echo WBPollHelper::show_backend_single_poll_widget_result( esc_html($poll_id), 'shortcode', 'text' );
-		}
-		
+		}?>
+		<script>
+			jQuery(document).ready(function() {
+			jQuery('#poll_seletect').change(function() {
+			  // Perform AJAX request here
+			  var pollid = jQuery(this).val();
+			const data = {
+				pollid: pollid,
+			};
 
+			var siteUrl =wbpollpublic.url;
+			jQuery.ajax({
+				url: siteUrl + '/wp-json/wbpoll/v1/listpoll/result/poll',
+				type: 'POST',
+				contentType: 'application/json',
+				data: JSON.stringify(data),
+				success: function (response) {
+					if (response.success) {
+						jQuery('.all_polll_result').html(response.result);
+					} else {
+						alert('Failed to unpublish poll.');
+					}
+				}
+			});
+			});
+		  });
+		</script>
+		<?php
 		echo $args['after_widget']; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 
